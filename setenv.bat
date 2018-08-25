92c990cc05d3c689ebffc5f6630b9367797bbec3
Jeremy <clough.jeremy@gmail.com>
2018-08-25 17:27:19 +0100

    Add scripts local directory to path

--- setenv.bat
+++ setenv.bat
@@ -2,8 +2,8 @@
 @if not "%ECHO%"=="" echo %ECHO%
 
 REM Description:
-REM	  Set Java working command-line environment, 
-REM   by setting JAVA_HOME, ANT_HOME and PATH. 
+REM   Setup a Java working environment, by setting JAVA_HOME, ANT_HOME 
+REM   and updating PATH. Also add local "scripts" directory to the path.
 REM
 REM Usage:
 REM   setenv.bat
@@ -20,22 +20,20 @@ if "%DEBUG%"=="1" (set TRACE=echo) else (set TRACE=rem)
 call %pwd%\ant\_ant_lib.bat :INIT "%pwd%"
 call %pwd%\java\_java_lib.bat :INIT "%pwd%"
 
+set PATH="%pwd%\scripts";%PATH%
+
 echo.
 echo Configured Java environment:
 echo ANT_HOME=%ANT_HOME%
 echo JAVA_HOME=%JAVA_HOME%
-rem echo PATH=%PATH%
 echo.
 
-REM Install local java if not aready installed.
 if not exist "%JAVA_HOME%" (
     echo Installing Java. Please wait...
     call %pwd%\scripts\install.bat java >nul
 )
 
-REM Install local ant if not already installed.
 if not exist "%ANT_HOME%" (
     echo Installing Ant. Please wait...
     call %pwd%\scripts\install.bat ant >nul
 )
-echo.
