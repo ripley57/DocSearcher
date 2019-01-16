88211b93613768a31dda726eb39fb1abf864d30b
Jeremy <ripley57@users.noreply.github.com>
2019-01-16 08:32:04 +0000

    Update ParseCmdLine.java

--- manifold/Eclipse/DocSearcher/src/main/java/docsearcher/core/ParseCmdLine.java
+++ manifold/Eclipse/DocSearcher/src/main/java/docsearcher/core/ParseCmdLine.java
@@ -8,22 +8,22 @@ public class ParseCmdLine
 	private static void usage()
 	{
 		System.err.println(
-			"																						\n" +
-		    "Description:																			\n" +
-			"   Automate the generation of Manifold-CF jobs for crawling filesytstem directories.	\n" + 
-			"																						\n" +
-			"Usage examples:																		\n" +
-			"																						\n" +
-			"   Create a Manifold filesystem repository connector:									\n" +
-			"      java -jar docsearcher.jar --repository \"name=repo1|type=filesystem\"			\n" +
-			"																						\n" +
-			"   Create a Manifold Solr output connector:											\n" +
-			"      java -jar docsearcher.jar --output \"name=output1|type=solr|core=core1\"			\n" +
-			"																						\n" +
-			"   Create a Manifold job specifying repository, output, and the filesystem paths:		\n" +
-			"      java -jar docsearcher.jar --job 													\n" +
-			"               \"repository=repo1|output=output1|path=d:/dir1|path=d:/dir2|depth=2\"	\n" +
-			"																						\n"
+		"											\n" +
+		"Description:										\n" +
+		"   Automate the generation of Manifold-CF jobs for crawling filesytstem directories.	\n" + 
+		"											\n" +
+		"Usage examples:									\n" +
+		"											\n" +
+		"   Create a Manifold filesystem repository connector:					\n" +
+		"      java -jar docsearcher.jar --repository \"name=repo1|type=filesystem\"		\n" +
+		"											\n" +
+		"   Create a Manifold Solr output connector:						\n" +
+		"      java -jar docsearcher.jar --output \"name=output1|type=solr|core=core1\"		\n" +
+		"											\n" +
+		"   Create a Manifold job specifying repository, output, and the filesystem paths:	\n" +
+		"      java -jar docsearcher.jar --job 							\n" +
+		"               \"repository=repo1|output=output1|path=d:/dir1|path=d:/dir2|depth=2\"	\n" +
+		"																						\n"
 		);
 	}
 	
@@ -32,8 +32,8 @@ public class ParseCmdLine
 		boolean invalidArgs = false; 
 		
 		LongOpt[] longopts = new LongOpt[4];
-		longopts[0] = new LongOpt("help", 		LongOpt.NO_ARGUMENT,		null, 'h');	// This option will return 'h'.
-		longopts[1] = new LongOpt("job", 		LongOpt.REQUIRED_ARGUMENT, 	null, 'j');	// This option will return 'j'.
+		longopts[0] = new LongOpt("help", 	LongOpt.NO_ARGUMENT,		null, 'h');	// This option will return 'h'.
+		longopts[1] = new LongOpt("job", 	LongOpt.REQUIRED_ARGUMENT, 	null, 'j');	// This option will return 'j'.
 		longopts[2] = new LongOpt("repository", LongOpt.REQUIRED_ARGUMENT, 	null, 'r');	// This option will return 'r'.
 		longopts[3] = new LongOpt("output", 	LongOpt.REQUIRED_ARGUMENT, 	null, 'o');	// This option will return 'o'.
 		
