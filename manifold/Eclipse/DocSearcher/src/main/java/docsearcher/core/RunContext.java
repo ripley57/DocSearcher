e4df25d2f007a70a2b48796eb50778c1f38e2565
Jeremy <ripley57@users.noreply.github.com>
2019-01-16 08:33:06 +0000

    Update RunContext.java

--- manifold/Eclipse/DocSearcher/src/main/java/docsearcher/core/RunContext.java
+++ manifold/Eclipse/DocSearcher/src/main/java/docsearcher/core/RunContext.java
@@ -7,41 +7,41 @@ import docsearcher.manifold.*;
 
 public class RunContext 
 {
-		private List<Job> jobs = new ArrayList<Job>();
-		private List<RepositoryConnector> repositories = new ArrayList<RepositoryConnector>();
-		private List<OutputConnector> outputs = new ArrayList<OutputConnector>();
+	private List<Job> jobs = new ArrayList<Job>();
+	private List<RepositoryConnector> repositories = new ArrayList<RepositoryConnector>();
+	private List<OutputConnector> outputs = new ArrayList<OutputConnector>();
 				
-		List<Job> getJobs() { return jobs; }
-		List<RepositoryConnector> getRepositories()	{ return repositories; }
-		List<OutputConnector> getOutputs() { return outputs; }
+	List<Job> getJobs() { return jobs; }
+	List<RepositoryConnector> getRepositories()	{ return repositories; }
+	List<OutputConnector> getOutputs() { return outputs; }
 				
-		public void addJob(String jobDesc) {
-			jobs.add(JobFactory.getJob(jobDesc));
-		}
+	public void addJob(String jobDesc) {
+		jobs.add(JobFactory.getJob(jobDesc));
+	}
 		
-		public void addRepository(String repositoryDesc) {
-			repositories.add(RepositoryFactory.getRepository(repositoryDesc));
-		}
+	public void addRepository(String repositoryDesc) {
+		repositories.add(RepositoryFactory.getRepository(repositoryDesc));
+	}
 		
-		public void addOutput(String outputDesc) {
-			outputs.add(OutputFactory.getOutput(outputDesc));
-		}
+	public void addOutput(String outputDesc) {
+		outputs.add(OutputFactory.getOutput(outputDesc));
+	}
 
-		public void dumpJobs() 
-		{
-			for (Job j : jobs)
-				System.err.println("" + j.toString());
-		}
+	public void dumpJobs() 
+	{
+		for (Job j : jobs)
+			System.err.println("" + j.toString());
+	}
 
-		public void dumpRepositories() 
-		{
-			for (RepositoryConnector r : repositories)
-				System.err.println("" + r.toString());
-		}
+	public void dumpRepositories() 
+	{
+		for (RepositoryConnector r : repositories)
+			System.err.println("" + r.toString());
+	}
 		
-		public void dumpOutputs() 
-		{
-			for (OutputConnector o : outputs)
-				System.err.println("" + o.toString());
-		}
+	public void dumpOutputs() 
+	{
+		for (OutputConnector o : outputs)
+			System.err.println("" + o.toString());
+	}
 }
