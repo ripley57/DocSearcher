1b11289601fcb8af9e77aad7d1b1aa4556cb3002
Jeremy <ripley57@users.noreply.github.com>
2019-01-16 08:15:50 +0000

    Update Main.java

--- manifold/Eclipse/DocSearcher/src/main/java/docsearcher/core/Main.java
+++ manifold/Eclipse/DocSearcher/src/main/java/docsearcher/core/Main.java
@@ -5,14 +5,14 @@ import org.apache.log4j.*;
 public class Main
 {
 	static Logger logger = Logger.getLogger(Main.class);
-	
+
 	private static String progName = "docsearcher";
-	
-    public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		logger.debug("DEBUG Main::main() Entering ...");
 		
 		RunContext ctx = new RunContext();
-        ParseCmdLine.parse(progName, args, ctx);
+		ParseCmdLine.parse(progName, args, ctx);
 		
 		ctx.dumpRepositories();
 		ctx.dumpOutputs();
